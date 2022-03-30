3bea317850eb9abb779a183a2efc08e162a668cf
Maltazard <markus.yarleque@gmail.com>
2022-03-30 14:54:05 -0500

    maskiuy

--- midgard.php
+++ midgard.php
@@ -56,20 +56,6 @@
                     <span style="--i:10;"></span>
                 </div>
             </td>
-            <td>
-                <div class="loader">
-                    <span style="--i:1;"></span>
-                    <span style="--i:2;"></span>
-                    <span style="--i:3;"></span>
-                    <span style="--i:4;"></span>
-                    <span style="--i:5;"></span>
-                    <span style="--i:6;"></span>
-                    <span style="--i:7;"></span>
-                    <span style="--i:8;"></span>
-                    <span style="--i:9;"></span>
-                    <span style="--i:10;"></span>
-                </div>
-            </td>
         </tr>
     </table>
     <footer>
