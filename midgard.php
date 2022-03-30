46c94d2a3f3cbcbec6b673f9c403b4ce7831ff7c
Maltazard <markus.yarleque@gmail.com>
2022-03-30 14:44:36 -0500

    mas

--- midgard.php
+++ midgard.php
@@ -53,6 +53,18 @@
         <span style="--i:10;"></span>
     </div>
 
+    <div class="loader">
+        <span style="--i:1;"></span>
+        <span style="--i:2;"></span>
+        <span style="--i:3;"></span>
+        <span style="--i:4;"></span>
+        <span style="--i:5;"></span>
+        <span style="--i:6;"></span>
+        <span style="--i:7;"></span>
+        <span style="--i:8;"></span>
+        <span style="--i:9;"></span>
+        <span style="--i:10;"></span>
+    </div>
     
     <footer>
         <p>Malta's Bot</p>
