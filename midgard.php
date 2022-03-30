7c0e22c39fefc569bd9bb0cdd5f3fb83544d88f2
Maltazard <markus.yarleque@gmail.com>
2022-03-30 16:02:34 -0500

    masul

--- midgard.php
+++ midgard.php
@@ -43,12 +43,8 @@
     <table style="border-spacing: 0px;" align="center">
         <tr>
             <td>
-                <a class="actual" href="https://discord.gg/CM9yAmXPfC" target=_blank><img src ="img/LOGOBOT.gif" style="width:70%;border-color: rgb(46, 152, 194);"></a>   
-            </td>
-        </tr>
-        <tr>
-            <td>
                 <div class="loader">
+                    <a class="actual" href="https://discord.gg/CM9yAmXPfC" target=_blank><img src ="img/LOGOBOT.gif" style="width:70%;border-color: rgb(46, 152, 194);"></a>   
                     <span style="--i:1;"></span>
                     <span style="--i:2;"></span>
                     <span style="--i:3;"></span>
