66dc8adfab71299ed6b887ac20820ce5ad117830
Maltazard <markus.yarleque@gmail.com>
2022-03-31 03:10:03 -0500

    masnnsn

--- info.php
+++ info.php
@@ -13,7 +13,7 @@
 <body>
 
 <center>
-    <table style="border: 2px ;border-spacing: 0px;border-color: black;border-style: solid;border-radius: 20px;" align="center" border = 0 align = center width = 50% height = 50% cellpadding = 10 cellspacing = 10>
+    <table style="border: 2px ;border-spacing: 20px;border-color: black;border-style: solid;border-radius: 20px;" align="center" border = 0 align = center width = 50% height = 50% cellpadding = 10 cellspacing = 10>
         
         <caption><b><i>Información</i></b></caption>
          
